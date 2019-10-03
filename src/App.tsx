07b02231ba92ebc3e99f3cdb5fdd03682264c103
maicazapanta <maicazapanta@github.com>
2019-10-03 16:26:32 +0800

    border-radius commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Slider } from "antd";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    inputValueTopLeft: 0,
+    inputValueTopRight: 0,
+    inputValueBottomLeft: 0,
+    inputValueBottomRight: 0
+  };
+
+  onChange = (value: any) => {
+    this.setState({
+      inputValueTopLeft: value
+    });
+  };
+  onChangeBottom = (value: any) => {
+    this.setState({
+      inputValueTopRight: value
+    });
+  };
+  onChangeLeft = (value: any) => {
+    this.setState({
+      inputValueBottomLeft: value
+    });
+  };
+  onChangeRight = (value: any) => {
+    this.setState({
+      inputValueBottomRight: value
+    });
+  };
+
+  render() {
+    const {
+      inputValueTopLeft,
+      inputValueTopRight,
+      inputValueBottomLeft,
+      inputValueBottomRight
+    } = this.state;
+
+    return (
+      <div>
+        <div
+          className="square"
+          style={{
+            borderTopLeftRadius: inputValueTopLeft,
+            borderTopRightRadius: inputValueTopRight,
+            borderBottomLeftRadius: inputValueBottomLeft,
+            borderBottomRightRadius: inputValueBottomRight
+          }}
+        ></div>
+        <Slider className="slider" defaultValue={0} onChange={this.onChange} />
+        <label className="top"> TOP LEFT: </label>
+        <input
+          className="slider1"
+          type="number"
+          min={0}
+          max={100}
+          style={{ marginLeft: 16 }}
+          value={inputValueTopLeft}
+          onChange={this.onChange}
+        />
+        <Slider
+          className="slider"
+          defaultValue={0}
+          onChange={this.onChangeBottom}
+        />
+        <label className="bottom"> TOP RIGHT: </label>
+        <input
+          className="slider1"
+          type="number"
+          min={0}
+          max={100}
+          style={{ marginLeft: 16 }}
+          value={inputValueTopRight}
+          onChange={this.onChange}
+        />
+        <Slider
+          className="slider"
+          defaultValue={0}
+          onChange={this.onChangeLeft}
+        />
+        <label className="left"> BOTTOM LEFT: </label>
+        <input
+          className="slider1"
+          type="number"
+          min={0}
+          max={100}
+          style={{ marginLeft: 16 }}
+          value={inputValueBottomLeft}
+          onChange={this.onChange}
+        />
+        <Slider
+          className="slider"
+          defaultValue={0}
+          onChange={this.onChangeRight}
+        />
+        <label className="right"> BOTTOM RIGHT: </label>
+        <input
+          className="slider1"
+          type="number"
+          min={0}
+          max={100}
+          style={{ marginLeft: 16 }}
+          value={inputValueBottomRight}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
